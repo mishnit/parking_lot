db0c54c98a0fa917e7ef618ddd9b575d20bfcbb8
mishnit <geekymishnit@gmail.com>
2020-06-16 10:24:47 +0530

    updated schema, message and service definitions

---
+++ parking/deploy/up.test.sql
@@ -0,0 +1,47 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE DATABASE IF NOT EXISTS parking_test;
+
+CREATE TABLE IF NOT EXISTS park_lots (
+  id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+  max_slot_num INT NOT NULL,
+  used_slot_count INT NOT NULL DEFAULT 0 CHECK (used_slot_count >= 0), CHECK (used_slot_count <= max_slot_num)
+);
+
+CREATE TABLE IF NOT EXISTS parks (
+  park_lot_id UUID REFERENCES park_lots,
+  slot_num INT NOT NULL UNIQUE,
+  car_num VARCHAR NOT NULL UNIQUE,
+  car_color VARCHAR NOT NULL,
+  PRIMARY KEY (park_lot_id, slot_num)
+);
+
+CREATE FUNCTION inc_used_slot() RETURNS TRIGGER
+AS $_$
+BEGIN
+UPDATE park_lots SET used_slot_count = used_slot_count + 1 WHERE id = NEW.park_lot_id;
+RETURN NEW;
+END $_$
+LANGUAGE 'plpgsql';
+
+CREATE FUNCTION dec_used_slot() RETURNS TRIGGER
+AS $_$
+BEGIN
+UPDATE park_lots SET used_slot_count = used_slot_count - 1  WHERE id = NEW.park_lot_id;
+RETURN NEW;
+END $_$
+LANGUAGE 'plpgsql';
+
+DROP TRIGGER IF EXISTS inc_used_slot_count ON parks;
+
+CREATE TRIGGER inc_used_slot_count -- trigger to increment used_slot_count in park_lots for inserts in parks
+  AFTER INSERT ON parks
+  FOR EACH ROW
+  EXECUTE PROCEDURE inc_used_slot();
+
+DROP TRIGGER IF EXISTS dec_used_slot_count ON parks;
+
+CREATE TRIGGER dec_used_slot_count -- trigger to decrement used_slot_count in park_lots for delete in parks
+  AFTER DELETE ON parks
+  FOR EACH ROW
+  EXECUTE PROCEDURE dec_used_slot();
